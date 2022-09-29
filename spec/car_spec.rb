853ac28d0a2d2bb8d8f6d06139cdcfe5ccbbd683
Shehroz Irfan <>
2022-09-29 12:58:44 +0500

    Skipping and Pending

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -9,9 +9,11 @@ describe 'Car' do
     end
 
     it "should allow read and write to :year" do
+      pending("It's pending due to some bugs")
       car = Car.new
       car.year = 2022
-      expect(car.year).to eq(2022)
+      # we've added 2021 to be expected, so that this test fails, as pending expects this test to be failed.
+      expect(car.year).to eq(2021)
     end
 
     it "should allow read and write to :color" do
@@ -21,11 +23,22 @@ describe 'Car' do
     end
 
     it "should allow reading for :wheels" do
+      # SKIPPING - adding skip to skip this block
+      # skip
+      # we can also add message to skip
+      skip("Need to debug this")
+
       car = Car.new
       expect(car.wheels).to eq(4)
     end
+
+    # PENDING - Omit the block
+    it "should allow writing to :doors"
   end
 
+  # SKIPPING - xdescribe to skip this block
+  # xdescribe '.colors' do
+  # we add '.' before the class methods
   describe '.colors' do
     it "should return an array of colors" do
       colors = ['blue', 'black', 'red', 'green']
@@ -33,6 +46,7 @@ describe 'Car' do
     end
   end
 
+  # we add '#' before the instance methods
   describe '#full_name' do
     it "should return a string with the expected format" do
       car = Car.new(make: "Honda", year: "2022", color: "white")
